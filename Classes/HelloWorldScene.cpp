4ac860f342ff1125b9a553e68602242e8199dc0d
fguan0529 <guan.fanny@gmail.com>
2016-08-15 14:38:45 -0700

    added background image woo

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -50,21 +50,19 @@ bool HelloWorld::init()
 
     /////////////////////////////
     // 3. add your codes below...
-
-    // add a label shows "Hello World"
-    // create and initialize a label
     
-    auto label = Label::createWithTTF("Hello World", "fonts/Marker Felt.ttf", 24);
+    auto label = Label::createWithTTF("Tic-Tac-Toe!", "fonts/Marker Felt.ttf", 50);
+    label->setColor(ccc3(0, 0, 0));
     
     // position the label on the center of the screen
     label->setPosition(Vec2(origin.x + visibleSize.width/2,
-                            origin.y + visibleSize.height - label->getContentSize().height));
+                            origin.y + visibleSize.height/2));
 
     // add the label as a child to this layer
     this->addChild(label, 1);
 
     // add "HelloWorld" splash screen"
-    auto sprite = Sprite::create("HelloWorld.png");
+    auto sprite = Sprite::create("tic-tac-toe_background.png");
 
     // position the sprite on the center of the screen
     sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));

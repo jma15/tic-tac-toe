8a502931ab3c1ca2b3b9cca28c3759715b159f47
Jeffrey Ma <jeffreyma@Jeffreys-MBP.hsd1.ca.comcast.net>
2016-09-06 12:17:12 -0700

    fixed variable optimization

--- Classes/game.h
+++ Classes/game.h
@@ -8,34 +8,7 @@
 class Game : public cocos2d::Layer
 {
 	cocos2d::Size visibleSize;
-	cocos2d::Sprite* panel1 = nullptr;
-	cocos2d::Sprite* panel2 = nullptr;
-	cocos2d::Sprite* panel3 = nullptr;
-	cocos2d::Sprite* panel4 = nullptr;
-	cocos2d::Sprite* panel5 = nullptr;
-	cocos2d::Sprite* panel6 = nullptr;
-	cocos2d::Sprite* panel7 = nullptr;
-	cocos2d::Sprite* panel8 = nullptr;
-	cocos2d::Sprite* panel9 = nullptr;
 	std::vector<cocos2d::Sprite*> spriteArray;
-
-	cocos2d::Sprite* panel10 = nullptr;
-	cocos2d::Sprite* panel11 = nullptr;
-	cocos2d::Sprite* panel12 = nullptr;
-	cocos2d::Sprite* panel13 = nullptr;
-	cocos2d::Sprite* panel14 = nullptr;
-	cocos2d::Sprite* panel15 = nullptr;
-	cocos2d::Sprite* panel16 = nullptr;
-    
-    cocos2d::Sprite* panel17 = nullptr;
-    cocos2d::Sprite* panel18 = nullptr;
-    cocos2d::Sprite* panel19 = nullptr;
-    cocos2d::Sprite* panel20 = nullptr;
-    cocos2d::Sprite* panel21 = nullptr;
-    cocos2d::Sprite* panel22 = nullptr;
-    cocos2d::Sprite* panel23 = nullptr;
-    cocos2d::Sprite* panel24 = nullptr;
-    cocos2d::Sprite* panel25 = nullptr;
     
     Player playerOne;
     Player playerTwo;

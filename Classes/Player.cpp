c24ec902b3655d97002640cb9d42d071dc9c4a81
fguan0529 <guan.fanny@gmail.com>
2016-09-11 00:29:35 -0700

    Fixed win check algorithm for diagonals and horizontals

--- Classes/Player.cpp
+++ Classes/Player.cpp
@@ -1,6 +1,7 @@
 #include "Player.h"
 #include "ui/CocosGUI.h"
 #include <string>
+#include <cmath>
 
 USING_NS_CC;
 
@@ -64,17 +65,52 @@ bool Player::didWin(int panel)
 bool Player::winGame(int panel, int increment)
 {
     int winCounter = 0;
-    int startPanel = panel;
+    int edgeCounter = 0;
 
+    int startPanel = panel;
+    int tempPanel = panel;
+    int tempPanelRow = 0;
+
+    // start from furthest back (totalMatch - 1) panels
     for (int i = -1 * (totalMatch - 1); i <= 0; i++)
     {
+        // set counters & startPanel index number
         winCounter = 0;
+        edgeCounter = 0;
         startPanel = panel + (i * increment);
+
+        // check if there are (totalMatch) panels in a row
         for (int j = 0; j < totalMatch; j++)
         {
-            if (isTaken(startPanel + (j * increment)))
-                winCounter++;
+            // current panel being checked & its row #
+            tempPanel = startPanel + (j * increment);
+            tempPanelRow = ceil(tempPanel / static_cast<double>(mode));
+
+            // if current panel isTaken by player
+            if (isTaken(tempPanel))
+            {
+                // if currently checking horizontal && is in same row, then win++
+                if (increment == 1 && tempPanelRow == ceil(panel / static_cast<double>(mode)))
+                    winCounter++; 
+                // else if checking diagonal && is an edge panel, then edge++ & win++
+                else if (increment == mode + 1 || increment == mode - 1)
+                {
+                    winCounter++;
+                    // check if panel is on the edge of board
+                    if (tempPanelRow == mode || tempPanelRow == 1
+                        || tempPanel % mode == 1 || tempPanel % mode == 0)
+                        edgeCounter++;
+                }
+                // else just win++
+                else
+                    winCounter++;
+            }
         }
+
+        // if there were more than 2 edges, panels not in same diagonal
+        if (edgeCounter > 2)
+            return false;
+        // if panels in a row == totalMatch, then win
         if (winCounter == totalMatch)
             return true;
     }
